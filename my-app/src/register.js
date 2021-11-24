2c36e87874f3290fafd83fc0b4d8c3fa5861adb3
Alpaslan Hamzaoğlu <44805408+alpaslanhamzaoglu@users.noreply.github.com>
2021-11-25 00:00:47 +0300

    forgot password and home files opened, link system addded to the login-register-home pages

--- my-app/src/register.js
+++ my-app/src/register.js
@@ -3,7 +3,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './register.css';
 import Axios from 'axios'
 import { Form, Button, Row, Container, Col } from 'react-bootstrap';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
+import { Link } from "react-router-dom";
 
 document.body.style.background = "#9caeff"
 
@@ -11,12 +12,12 @@ function Register() {
 
     const [uemail, setMail] = useState("");
     const [password, setPassword] = useState("");
-    const[pass_repeat, setPassRepeat] = useState("");
+    const [pass_repeat, setPassRepeat] = useState("");
 
     const submitMail = () => {
-      //alttaki conditiona tekrar bak ilerde
-      if(pass_repeat == password)
-          Axios.post("http://localhost:3001/register", { uemail: uemail, password: password, pass_repeat: pass_repeat }).then(() => { alert("successful"); });
+        //alttaki conditiona tekrar bak ilerde
+        if (pass_repeat === password)
+            Axios.post("http://localhost:3001/register", { uemail: uemail, password: password, pass_repeat: pass_repeat }).then(() => { alert("successful"); });
     };
 
     return (
@@ -27,7 +28,7 @@ function Register() {
                     <div class="col align-self-center" className="gee"><svg xmlns="http://www.w3.org/2000/svg" width="4%" height="auto" fill="rgb(144, 0, 0)" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
                         <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" /> </svg>
                     </div>
-                    <div class="col align-self-center" className="p1">AGA</div>
+                    <Link to="/home" style={{ textDecoration: 'none' }}><div class="col align-self-center" className="p1">AGA</div></Link>
                     <div class="col align-self-center" className="p2">Car Pooling System</div>
                 </div>
             </div>
