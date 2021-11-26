2f498b8e0877226be2a7e46753d739c2cc711ea2
Alpaslan Hamzaoğlu <44805408+alpaslanhamzaoglu@users.noreply.github.com>
2021-11-26 19:34:55 +0300

    i truely dont remember, sorry :/

--- my-app/src/profile.js
+++ my-app/src/profile.js
@@ -1,7 +1,7 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from "react-router-dom";
-import { Form, Button, Row, Container, Col, FloatingLabel } from 'react-bootstrap';
+import { Form, Button, Row, Container, Col, FloatingLabel, Table } from 'react-bootstrap';
 import { useState, useEffect } from 'react';
 import Axios from 'axios'
 import './profile.css';
@@ -25,9 +25,8 @@ function Profile() {
         dataPass(data[0].password);
         dataMail(data[0].uemail);
         dataVac(data[0].covidvac);
-        if (vac == 1) {
+        if (vac == 2) {
             dataVac("Vac");
-            console.log("nope")
         }
         dataInfo(data[0].infos);
     }
@@ -56,16 +55,16 @@ function Profile() {
             <div>
                 <Container id="infos">
                     <Row className="justify-content-md-center">
-                        <Col xs lg="3">Your name is {name}.</Col>
+                        <Col xs lg="12">Your name is {name}.</Col>
                     </Row>
                     <Row className="justify-content-md-center">
-                        <Col xs lg="3">Your email is {mail}.</Col>
+                        <Col xs lg="12">Your email is {mail}.</Col>
                     </Row>
                     <Row className="justify-content-md-center">
-                        <Col xs lg="3">Your info is {info}.</Col>
+                        <Col xs lg="12">Your info is {info}.</Col>
                     </Row>
                     <Row className="justify-content-md-center">
-                        <Col xs lg="3">Your covid vaction id is {vac}.</Col>
+                        <Col xs lg="12">Your covid vaction id is {vac}.</Col>
                     </Row>
                 </Container>
             </div>
