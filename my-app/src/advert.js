f700dd20d2bc19100921f7a534b68676a8a80f28
goktugcaliskan <goktugcaliskan@sabanciuniv.edu>
2021-11-29 00:29:45 +0300

    advert listing nodes are created

--- my-app/src/advert.js
+++ my-app/src/advert.js
@@ -4,17 +4,18 @@ import { Card, Button, Row, Container, Col, FloatingLabel } from 'react-bootstra
 function Advert(props) {
   return (
     <advert>
-      <Card className="text-center">
-      <Card.Header>{props.advert.departure} - {props.advert.destination}</Card.Header>
-      <Card.Body>
-      <Card.Title>Special title treatment</Card.Title>
-      <Card.Text>
-        With supporting text below as a natural lead-in to additional content.
-      </Card.Text>
-      <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-      <Card.Footer className="text-muted">2 days ago</Card.Footer>
+      <Card className="text-center" style={{ width: '50rem' }}>
+        <Card.Header>{props.advert.departure} - {props.advert.destination}</Card.Header>
+        <Card.Body>
+          <Card.Title>{props.advert.deptime} - {props.advert.arrtime}</Card.Title>
+          <Card.Text>
+            {props.advert.carmodel}
+          </Card.Text>
+          <Button variant="primary">Purchase</Button>
+        </Card.Body>
+        <Card.Footer className="text-muted">2 days ago</Card.Footer>
       </Card>
+      <br />
     </advert>
   )
 }
--- my-app/src/advertlisting.js
+++ my-app/src/advertlisting.js
@@ -37,7 +37,7 @@ function AdvertListing() {
                 </div>
             </div>
 
-            <div className="adverts">
+            <div className="Adverts" id="advertss">
                 {(adverts != null) ? adverts.map((advert, index) => <Advert key={index} advert={advert} />) : ''}
             </div>
             
