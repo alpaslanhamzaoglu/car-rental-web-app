92d4e29761d506e9d280a2ff987c1525a9888d42
erincayaz <45971992+erincayaz@users.noreply.github.com>
2021-11-29 00:19:15 +0300

    push

--- my-app/src/advert.js
+++ my-app/src/advert.js
@@ -5,16 +5,17 @@ function Advert(props) {
   return (
     <advert>
       <Card className="text-center">
-      <Card.Header>{props.advert.departure} - {props.advert.destination}</Card.Header>
-      <Card.Body>
-      <Card.Title>Special title treatment</Card.Title>
-      <Card.Text>
-        With supporting text below as a natural lead-in to additional content.
-      </Card.Text>
-      <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-      <Card.Footer className="text-muted">2 days ago</Card.Footer>
+        <Card.Header>{props.advert.departure} - {props.advert.destination}</Card.Header>
+        <Card.Body>
+          <Card.Title>{props.advert.deptime} - {props.advert.arrtime}</Card.Title>
+          <Card.Text>
+            {props.advert.carmodel}
+          </Card.Text>
+          <Button variant="primary">Purchase</Button>
+        </Card.Body>
+        <Card.Footer className="text-muted">2 days ago</Card.Footer>
       </Card>
+      <br />
     </advert>
   )
 }
